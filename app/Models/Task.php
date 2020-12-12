fb30cfb31e1ab263e70d251348b835c4165577cc
iman sadafi <sadafiiman@gmail.com>
2020-12-12 22:04:50 +0330

    create model & migration task & added task to user model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable=[
+        'user_id',
+        'title',
+        'done'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+
 }
