50d2b5d30864ee2053371b5387ba64c2cc7314ab
iman sadafi <sadafiiman@gmail.com>
2020-12-13 19:29:01 +0330

    added bootstrap ui

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+
+    public function index()
+    {
+        $tasks=Task::all();
+
+        return view('tasks.index')->withTasks($tasks);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Task $task)
+    {
+        //
+    }
+
+
+    public function edit(Task $task)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
